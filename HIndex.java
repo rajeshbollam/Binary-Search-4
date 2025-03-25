ac091f10c56a27276490cb9bf518ee9b67af97b2
Rajesh <rbollam24@gmail.com>
2025-03-25 10:30:11 -0400

    added tc and sc

--- HIndex.java
+++ HIndex.java
@@ -1,6 +1,8 @@
 //The idea here is to use Binary Search to find the h-index
 //At each mid, we check if the difference between n and mid is equal to the value in array, if yes, that's the h-index, if difference is greater, then low goes to mid + 1, else high goes to mid - 1
 //At the end when both low and high cross, then h-index will be at index low with value of diff there, which is n-low
+//Time Complexity: O(log n)
+//Space Complexity: O(1)
 class Solution {
     public int hIndex(int[] citations) {
         int n = citations.length;
